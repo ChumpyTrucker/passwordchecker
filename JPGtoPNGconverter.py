1e41c8dfb1235149f64aa705885abd10152f4b06
ChumpyTrucker <87389714+ChumpyTrucker@users.noreply.github.com>
2023-07-04 22:21:31 +0100

    Send an email with python.

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,13 @@
+import sys
+import os 
+from PIL import Image
+
+# grab first and second argument
+
+# check is new/ exists, if not create 
+
+
+
+# loop through Pokede,
+# convert images to png
+# save to the new folder
